84aab97baca4de2ce0e9dcc154bfc1611fee21e4
martinb <martinbraun123@aol.com>
2023-01-07 16:50:00 +0100

    minor

--- test/certs/recreate_server_certs.sh
+++ test/certs/recreate_server_certs.sh
@@ -5,4 +5,4 @@ rm -r admin
 bash gen_server_cert.sh root_ca monitor/postgres_server monitor unused
 bash gen_server_cert.sh root_ca postgres/postgres_server postgres unused
 # cert is named "postgres_server", but works
-bash gen_server_cert.sh root_ca admin/postgres_server postgres unused
+bash gen_server_cert.sh root_ca admin/postgres_server admin unused
