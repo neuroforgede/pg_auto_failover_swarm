a48f55c33780794a948c8d2f7a51832a458bf7fa
martinb <martinbraun123@aol.com>
2023-01-07 15:59:31 +0100

    remove replica numbers from certificates

--- test/certs/recreate_server_certs.sh
+++ test/certs/recreate_server_certs.sh
@@ -1,7 +1,5 @@
 rm -r monitor
-rm -r node_1
-rm -r node_2
+rm -r postgres
 
-bash gen_server_cert.sh root_ca monitor/postgres_server monitor.local unused
-bash gen_server_cert.sh root_ca node_1/postgres_server node1.local unused
-bash gen_server_cert.sh root_ca node_2/postgres_server node2.local unused
+bash gen_server_cert.sh root_ca monitor/postgres_server monitor unused
+bash gen_server_cert.sh root_ca postgres/postgres_server postgres unused
