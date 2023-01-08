79dcf8b0e1a2ca2c0db32de9c594de868cc856cd
martinb <martinbraun123@aol.com>
2023-01-08 18:52:31 +0100

    add conditional setup to not kill existing setups

--- pgaf/pgaf.sh
+++ pgaf/pgaf.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
+log() {
+  echo "[pgaf-swarm] $1"
+}
+
+error() {
+  log "Error: $1"
+}
+
 check_result () {
     ___RESULT=$?
     if [ $___RESULT -ne 0 ]; then
-        echo $1
+        error $1
         exit 1
     fi
+}
+
+docker_setup_env() {
+	declare -g DATABASE_ALREADY_EXISTS
+	if [ -s "/var/lib/postgresql/.config/pg_autoctl/var/lib/postgresql/data/cluster/pg_autoctl.cfg" ]; then
+		DATABASE_ALREADY_EXISTS='true'
+    log "cluster is already initialized, skipping setup..."
+  else
+    ls -la /var/lib/postgresql/data/cluster
+	fi
 }
 
 # base structure of this file is from https://gist.github.com/nicosingh/9583fe487bddf853c38a5b8c9db2cf7a
@@ -13,59 +31,63 @@ check_result () {
 # initially from from https://github.com/docker-library/postgres/blob/master/15/bullseye/docker-entrypoint.sh
 # used to create initial postgres directories and if run as root, ensure ownership to the "postgres" user
 docker_create_db_directories() {
-  local user
-  user="$(id -u)"
+  if [ -z "$DATABASE_ALREADY_EXISTS" ]; then
+    local user
+    user="$(id -u)"
 
-  mkdir -p "/var/lib/postgresql/data"
-  # ignore failure since there are cases where we can't chmod (and PostgreSQL might fail later anyhow - it's picky about permissions of this directory)
-  chmod 700 "/var/lib/postgresql/data" || :
+    mkdir -p "/var/lib/postgresql/data/cluster"
 
-  mkdir -p /var/lib/postgresql/.postgresql
-  ln -s /var/run/secrets/ssl_key /var/lib/postgresql/.postgresql/postgresql.key
-  ln -s /var/run/secrets/ssl_cert /var/lib/postgresql/.postgresql/postgresql.crt
-  chown -R postgres:postgres /var/lib/postgresql/.postgresql
+    mkdir -p /var/lib/postgresql/.postgresql
+    ln -s /var/run/secrets/ssl_key /var/lib/postgresql/.postgresql/postgresql.key
+    ln -s /var/run/secrets/ssl_cert /var/lib/postgresql/.postgresql/postgresql.crt
+    chown -R postgres:postgres /var/lib/postgresql/.postgresql
 
-  mkdir -p /var/lib/postgresql/data/cluster
-  chown -R postgres:postgres /var/lib/postgresql/data
+    mkdir -p /var/lib/postgresql/data/cluster
+    chown -R postgres:postgres /var/lib/postgresql/data
 
-  # ignore failure since it will be fine when using the image provided directory; see also https://github.com/docker-library/postgres/pull/289
-  mkdir -p /var/run/postgresql || :
-  chmod 775 /var/run/postgresql || :
+    # ignore failure since it will be fine when using the image provided directory; see also https://github.com/docker-library/postgres/pull/289
+    mkdir -p /var/run/postgresql || :
+    chmod 775 /var/run/postgresql || :
+  fi
 }
 
 create_monitor() {
-  echo "creating data monitor..."
-  # FIXME: check if already initialized
-  sudo -u postgres /usr/bin/pg_autoctl create monitor \
-    --pgdata /var/lib/postgresql/data/cluster \
-    --pgctl /usr/lib/postgresql/14/bin/pg_ctl \
-    --hostname "${PGAF_HOSTNAME}" \
-    --skip-pg-hba \
-    --ssl-ca-file "$PGAF_SSL_CA" \
-    --server-cert "$PGAF_SSL_CERT" \
-    --server-key "$PGAF_SSL_KEY" \
-    --ssl-mode "$PGAF_SSL_MODE"
-  check_result "failed to create monitor"
-  # FIXME: check error codes
+  if [ -z "$DATABASE_ALREADY_EXISTS" ]; then
+    log "creating data monitor..."
+    # FIXME: check if already initialized
+    sudo -u postgres /usr/bin/pg_autoctl create monitor \
+      --pgdata /var/lib/postgresql/data/cluster \
+      --pgctl /usr/lib/postgresql/14/bin/pg_ctl \
+      --hostname "${PGAF_HOSTNAME}" \
+      --skip-pg-hba \
+      --ssl-ca-file "$PGAF_SSL_CA" \
+      --server-cert "$PGAF_SSL_CERT" \
+      --server-key "$PGAF_SSL_KEY" \
+      --ssl-mode "$PGAF_SSL_MODE"
+    check_result "failed to create monitor"
+    # FIXME: check error codes
+  fi
 }
 
 create_postgres() {
-  echo "creating data node..."
-  # FIXME: check if already initialized
-  # TODO: make monitor url configurable
-  sudo -u postgres /usr/bin/pg_autoctl create postgres \
-    --pgdata /var/lib/postgresql/data/cluster \
-    --monitor "postgres://autoctl_node@${PGAF_MONITOR_HOSTNAME}:5432/pg_auto_failover" \
-    --hostname "${PGAF_HOSTNAME}" \
-    --name "${PGAF_NAME}" \
-    --pgctl /usr/lib/postgresql/14/bin/pg_ctl \
-    --skip-pg-hba \
-    --ssl-ca-file "$PGAF_SSL_CA" \
-    --server-cert "$PGAF_SSL_CERT" \
-    --server-key "$PGAF_SSL_KEY" \
-    --ssl-mode "$PGAF_SSL_MODE"
-  check_result "failed to create postgres"
-  # FIXME: check error codes
+  if [ -z "$DATABASE_ALREADY_EXISTS" ]; then
+    log "creating data node..."
+    # FIXME: check if already initialized
+    # TODO: make monitor url configurable
+    sudo -u postgres /usr/bin/pg_autoctl create postgres \
+      --pgdata /var/lib/postgresql/data/cluster \
+      --monitor "postgres://autoctl_node@${PGAF_MONITOR_HOSTNAME}:5432/pg_auto_failover" \
+      --hostname "${PGAF_HOSTNAME}" \
+      --name "${PGAF_NAME}" \
+      --pgctl /usr/lib/postgresql/14/bin/pg_ctl \
+      --skip-pg-hba \
+      --ssl-ca-file "$PGAF_SSL_CA" \
+      --server-cert "$PGAF_SSL_CERT" \
+      --server-key "$PGAF_SSL_KEY" \
+      --ssl-mode "$PGAF_SSL_MODE"
+    check_result "failed to create postgres"
+    # FIXME: check error codes
+  fi
 }
 
 setup_pg_ident_conf() {
@@ -99,7 +121,7 @@ setup_hba_node() {
 }
 
 pg_autoctl_run() {
-  echo "starting postgres..."
+  log "starting pg_autoctl..."
   exec sudo -u postgres /usr/bin/pg_autoctl run --pgdata /var/lib/postgresql/data/cluster
 }
 
@@ -117,6 +139,7 @@ run_sql() {
 
 case "$1" in
 monitor)
+  docker_setup_env
   docker_create_db_directories
   create_monitor
   setup_pg_ident_conf
@@ -125,6 +148,7 @@ monitor)
   pg_autoctl_run
   ;;
 db-server)
+  docker_setup_env
   docker_create_db_directories
   create_postgres
   setup_pg_ident_conf
@@ -137,7 +161,7 @@ psql)
   run_sql "$@"
   ;;
 *)
-  echo "command '${@}' not supported"
+  error "command '${@}' not supported"
   exit 1
   ;;
 esac
