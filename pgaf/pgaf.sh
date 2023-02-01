74aa3c0a7da61064cf9e8c611c9d30f76541b83c
Martin Braun <braun@neuroforge.de>
2023-02-02 00:24:39 +0100

    minor

--- pgaf/pgaf.sh
+++ pgaf/pgaf.sh
@@ -131,8 +131,9 @@ run_standalone() {
   # precompiled binaries and for testing
   # in docker-compose stacks locally where
   # it does not make sense to spin up a multinode cluster
-  PG_MAJOR=$POSTGRES_MAJOR_VERSION \ 
-    exec docker-entrypoint.sh "$@"
+  export PATH="$PATH:/usr/lib/postgresql/$POSTGRES_MAJOR_VERSION/bin"
+  export PG_MAJOR=$POSTGRES_MAJOR_VERSION
+  exec docker-entrypoint.sh "$@"
 }
 
 case "$1" in
